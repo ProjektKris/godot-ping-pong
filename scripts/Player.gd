96407eb830d4ac0de8c53d80d824426725e93de0
ProjektKris <rbxkrisxyz@gmail.com>
2021-10-10 20:31:29 +0700

    added player

---
+++ scripts/Player.gd
@@ -0,0 +1,34 @@
+extends StaticBody2D
+
+export(bool) var player1 = true
+export(float) var velocity = 300.0
+
+var up := false
+var down := false
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	if up:
+		if position.y > 50:
+			position.y -= velocity * delta
+	if down:
+		if position.y < 550:
+			position.y += velocity * delta 
+	pass
+
+# Called when the client inputs
+func _input(event: InputEvent) -> void:
+	var player_id = "1" if player1 else "2"
+	if event.is_action_pressed("player" + player_id + "_up"):
+		up = true
+	elif event.is_action_pressed("player" + player_id + "_down"):
+		down = true
+	elif event.is_action_released("player" + player_id + "_up"):
+		up = false
+	elif event.is_action_released("player" + player_id + "_down"):
+		down = false
+	pass
